6435ea49e03e7ba4e72ff0262c44925ccd151196
SimplyPancake <danielduikvis@gmail.com>
2019-01-10 17:21:56 +0100

    Main init
    Ik heb een beetje opgeschreven om de logica te beschrijven.

---
+++ stoplichten/stoplichten.ino
@@ -0,0 +1,24 @@
+void setup() {
+  // Welke pins gaan we gebruiken?
+  
+  //Dit is een voorbeeld:
+
+
+  //In een array zit een array. Eerst wordt de GND pin gegeven, dan die voor het groene LED,
+  //Daarna voor het rode LED.
+  //Bijv stoplicht 3 heeft GND-pin = 1, RLEd-pin = 5 en GLED-pin = 8:
+  //stoplicht[2][0] = 1, stoplicht[2][1] = 5 en stoplicht[2][2] = 8
+  int stoplicht[] = {
+    {},
+    {},
+    {},
+    {}};
+//  stoplicht[0]
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
