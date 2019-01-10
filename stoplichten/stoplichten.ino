813362cf50abf8e7f356a97492043be42aadff67
SimplyPancake <danielduikvis@gmail.com>
2019-01-10 20:22:40 +0100

    Kleine beschrijving van stoplichten
    de titel zegt het

--- stoplichten/stoplichten.ino
+++ stoplichten/stoplichten.ino
@@ -4,12 +4,10 @@ void setup() {
   //Bijv stoplicht 3 heeft GND-pin = 1, RLEd-pin = 5 en GLED-pin = 8:
   //stoplicht[2][0] = 1, stoplicht[2][1] = 5 en stoplicht[2][2] = 8
   int stoplicht[] = {
-    {},
-    {},
-    {},
-    {}};
-//  stoplicht[0]
-
+    {},   //Stoplict 1
+    {},   //Stoplicht 2
+    {},   //Stoplicht 3
+    {}};  //Stoplicht 4
 
 }
 
