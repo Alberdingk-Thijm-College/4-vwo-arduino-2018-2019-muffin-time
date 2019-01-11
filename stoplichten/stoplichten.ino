3ffce0fcc7dd96b798d3fd5ccd359728f8353435
SimplyPancake <danielduikvis@gmail.com>
2019-01-11 11:56:05 +0100

    Pinmode getallen
    De pin-getallen voor de LED-lampen zijn erin gezet!

--- stoplichten/stoplichten.ino
+++ stoplichten/stoplichten.ino
@@ -4,10 +4,10 @@ void setup() {
   //Bijv stoplicht 3 heeft GND-pin = 1, RLEd-pin = 5 en GLED-pin = 8:
   //stoplicht[2][0] = 1, stoplicht[2][1] = 5 en stoplicht[2][2] = 8
   int stoplicht[] = {
-    {},   //Stoplict 1
-    {},   //Stoplicht 2
-    {},   //Stoplicht 3
-    {}};  //Stoplicht 4
+    {GND, 3, 2},   //Stoplict 1
+    {GND, 5, 4},   //Stoplicht 2
+    {GND, 7, 6},   //Stoplicht 3
+    {GND, 9, 8}};  //Stoplicht 4
 
 }
 
