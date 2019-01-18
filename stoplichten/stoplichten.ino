5b6d9e7b0d97d3f6653e4d57b07249efa1d4f963
SimplyPancake <danielduikvis@gmail.com>
2019-01-18 12:06:06 +0100

    Stoplichtcycle werkt

--- stoplichten/stoplichten.ino
+++ stoplichten/stoplichten.ino
@@ -1,4 +1,3 @@
-
   //In een array zit een array. Eerst wordt de GND pin gegeven, dan die voor het groene LED,
   //Daarna voor het rode LED.
   //Bijv stoplicht 3 heeft GND-pin = GND, RLEd-pin = 5 en GLED-pin = 8:
@@ -9,34 +8,41 @@
     {7, 6},   //Stoplicht 3
     {9, 8}};  //Stoplicht 4
 
-  //Alle pin-getallen:
-  int pins[8] = {stoplicht[0][0], stoplicht[0][1], stoplicht[1][0], stoplicht[1][1], stoplicht[2][0], stoplicht[2][1], stoplicht[3][0], stoplicht[3][1]};
+  //Alle pin-getallen (gesorteerd van laag naar hoog):
+  int pins[8] = {stoplicht[0][1], stoplicht[0][0], stoplicht[1][1], stoplicht[1][0], stoplicht[2][1], stoplicht[2][0], stoplicht[3][1], stoplicht[3][0]};
+  int pinsRood[4] = {stoplicht[0][0], stoplicht[1][0], stoplicht[2][0], stoplicht[3][0]};
 
-  
+
   //We willen alle pins out.
   int outMin = 2; // Lowest input pin
   int outMax = 9; // Highest input pin
 
 void setup() {
   Serial.begin(115200);
+//  pinMode(3, OUTPUT);
+
+
+//Alle pins op output:
+  for(int i = 0; i <= sizeof(pins); i++) {
+    pinMode(pins[i], OUTPUT);
+    Serial.println(i);
+  }
   
-  for(int i = outMin; i < outMax; i++) {
-    pinMode(i, OUTPUT);
-  }
-
-  for(int k = 0; k < sizeof(pins); k++) {
-  Serial.println(pins[k]);
-  }
-
+  
 }
 
 void loop() {
-  for (int j = outMin; j < outMax; j++) {
+
+//  Loop door alle stoplichten:
+  for (int j = outMin; j <= outMax; j++) {
     if (j > outMax) {
       j = outMin;
     }
-    pinMode(j, HIGH);
+    Serial.println(j);
+    digitalWrite(j, HIGH);
     delay(1000);
-    pinMode(j, LOW);
+    digitalWrite(j, LOW);
    }
+
+  
 }
