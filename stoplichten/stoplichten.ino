9abbbccb477b6b9a67462b84324ffe7fb410b9bc
SimplyPancake <danielduikvis@gmail.com>
2019-01-19 23:35:44 +0100

    bugfix

--- stoplichten/stoplichten.ino
+++ stoplichten/stoplichten.ino
@@ -57,7 +57,7 @@ void loop() {
 //   }
 
   for (int t = 0; t <=  3; t++) {
-    pinMode(pinsRood[t], HIGH);
+    pinMode(pinsRood[t], OUTPUT);
     digitalWrite(pinsRood[t], HIGH);
     pinsRoodMode[t] = 1;
   }
